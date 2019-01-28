1f56165621501d21f2eb5efae4ee1a4aebc81d90
Toinn <ngoctoi.it@gmail.com>
2019-01-29 01:15:44 +0700

    update action editor video

--- src/app/views/videos/video-editor.component.ts
+++ src/app/views/videos/video-editor.component.ts
@@ -3,6 +3,9 @@ import {environment} from '../../../environments/environment.prod';
 import {VideoService} from '../../services/video.service';
 import {VideoPaginate} from '../../model/video-paginate.model';
 import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth-service.service';
+import {ConfirmationDialogService} from '../confirmation-dialog/confirmation-dialog.service';
+import {HttpErrorResponse} from '@angular/common/http';
 @Component({
     selector: 'app-video-editor',
     templateUrl: './video-editor.component.html',
@@ -11,15 +14,21 @@ export class VideoEditorComponent implements OnInit {
 
     environment: any;
     videos: VideoPaginate;
+    selectItem = false;
+    selected: any;
+    role: string;
     constructor(
         private videoService: VideoService,
-        private router: Router
+        private router: Router,
+        private authService: AuthService,
+        private confirmationDialogService: ConfirmationDialogService,
     ) {
         this.environment = environment;
     }
 
     ngOnInit() {
         this.getEditorVideo();
+        this.role = this.authService.getRoleUser();
     }
 
     getEditorVideo() {
@@ -37,5 +46,28 @@ export class VideoEditorComponent implements OnInit {
 
     eventViewDetailVideo(id) {
         this.router.navigate(['videos/edit', id], { queryParams: id, skipLocationChange: true});
+    }
+
+    isActive(item) {
+        return this.selected === item;
+    }
+
+    selectVideo(item) {
+        this.selectItem = true;
+        this.selected = item;
+    }
+
+    changeItemToPublish() {
+        this.confirmationDialogService.confirm('Xác nhận', 'Gửi lên xuất bản ?')
+            .then((confirmed) => {
+                if (confirmed) {
+                    this.videoService.changeVideoToPublish(this.selected.id).then(res => {
+                        this.getEditorVideo();
+                    }, (errorRes: HttpErrorResponse) => {
+                    });
+                }
+            })
+            .catch(() => {
+            });
     }
 }

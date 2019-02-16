4e0655ec078c7aaecc3a588e18e6b1b7a1d13e77
Toinn <ngoctoi.it@gmail.com>
2019-02-16 12:34:06 +0700

    update code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: true,
     hls: false,
     server_hls : 'http://192.168.1.67:1935/vod/mp4:',
-    api_url: 'http://api-vtv-world.local.ub/api/v1',
-    storage_url: 'http://api-vtv-world.local.ub/storage/',
+    api_url: 'http://apiworld.vtv.vn/api/v1',
+    storage_url: 'http://apiworld.vtv.vn/storage/',
     pusher: {
         key: '8db9afe1b28c36e74654',
         cluster: 'ap1',
